ab6aa11545ef6efc755c64c3ceaa214e42c74bdd
Christopher <cuadradochris@hotmail.com>
2022-12-06 16:32:25 -0500

    commit before installs

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.css'
+
+export const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className='sidebar_header'></div>
+        <div className='sidebar_search'></div>
+        <div className='sidebar_chats'></div>
+    </div>
+  )
+}
