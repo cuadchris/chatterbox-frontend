d0bd46db34520d1045c9ded2ec22c3333ad35111
Christopher <cuadradochris@hotmail.com>
2022-12-06 17:45:22 -0500

    search bar added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,19 +4,25 @@ import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import ChatIcon from "@mui/icons-material/Chat";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Avatar, IconButton } from "@mui/material";
+import { SearchOutlined } from "@mui/icons-material";
 
 export const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar_header">
-        <Avatar />
+        <Avatar src="https://cdn-icons-png.flaticon.com/512/147/147142.png" />
         <div className="sidebar_header_right">
           <DonutLargeIcon />
           <ChatIcon />
           <MoreVertIcon />
         </div>
       </div>
-      <div className="sidebar_search"></div>
+      <div className="sidebar_search">
+        <div className="sidebar_search_container">
+            <SearchOutlined/>
+            <input placeholder="Search or begin new chat" />
+        </div>
+      </div>
       <div className="sidebar_chats"></div>
     </div>
   );
