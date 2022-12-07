365e7a2a86f14310bba1917ae0764bf3d5a8e5ff
Christopher <cuadradochris@hotmail.com>
2022-12-06 20:29:04 -0500

    footer component. having trouble targeting ui icon

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { AttachFile, MoreVert, SearchOutlined } from "@mui/icons-material";
+import {
+  AttachFile,
+  InsertEmoticon,
+  MoreVert,
+  SearchOutlined,
+} from "@mui/icons-material";
 import { Avatar } from "@mui/material";
+import MicIcon from "@mui/icons-material/Mic";
 import "./Chat.css";
 
 export const Chat = () => {
@@ -32,16 +38,24 @@ export const Chat = () => {
           <span className="chat_timestamp">{new Date().toUTCString()}</span>
         </p>
         <p className="chat_message chat_receiver">
-          <span className="chat_name">Brandt</span>This is a message back thats also pointless
+          <span className="chat_name">Brandt</span>This is a message back thats
+          also pointless
           <span className="chat_timestamp">{new Date().toUTCString()}</span>
         </p>
         <p className="chat_message">
-          <span className="chat_name">Chris</span>This is a message again
-          again buddy
+          <span className="chat_name">Chris</span>This is a message again again
+          buddy
           <span className="chat_timestamp">{new Date().toUTCString()}</span>
         </p>
       </div>
-      <div className="chat_footer"></div>
+      <div className="chat_footer">
+        <InsertEmoticon className="icon" />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon className="icon" />
+      </div>
     </div>
   );
 };
