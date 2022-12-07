98d1da89c5182fb8e2e8850961d8250eaccbe5dd
Christopher <cuadradochris@hotmail.com>
2022-12-07 01:31:09 -0500

    pusher added to front-end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
 import "./App.css";
 import { Chat } from "./components/Chat";
 import { Sidebar } from "./components/Sidebar";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const pusher = new Pusher("0b050d35b8ed03689cee", {
+      cluster: "us2",
+    });
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      setMessages([...messages, data]);
+    });
+    return () => {
+      channel.unbind_all()
+      channel.unsubscribe()
+    }
+  }, [messages]);
+
+  console.log(messages)
+
   return (
     <div className="App">
       <div className="app_body">
         <Sidebar />
-        <Chat/>
+        <Chat />
       </div>
     </div>
   );
